79b96e8d812baf9e5a812a4857d85f72f089d793
silencecork <silencecork@gmail.com>
2019-05-17 01:53:13 +0800

    add simple api that can connect to redis

--- src/routes.php
+++ src/routes.php
@@ -14,4 +14,33 @@ return function (App $app) {
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
+    $app->group('/api', function () use ($app, $container) {
+        $app->get('/put-redis', function (Request $request, Response $response, array $args) use ($container) {
+            $key = $request->getParam('key');
+            $value = $request->getParam('value');
+
+            $redisHost = getenv('REDIS_HOST');
+
+            $client = new Predis\Client(['host' => $redisHost, 'port' => 6379]);
+            $client->connect();
+            $result = $client->set($key, $value);
+            $client->disconnect();
+
+            return $response->withJson($result);
+        });
+
+        $app->get('/get-redis', function (Request $request, Response $response, array $args) use ($container) {
+            $key = $request->getParam('key');
+
+            $redisHost = getenv('REDIS_HOST');
+
+            $client = new Predis\Client(['host' => $redisHost, 'port' => 6379]);
+            $client->connect();
+            $value = $client->get($key);
+            $client->disconnect();
+
+            return $response->withJson($value);
+        });
+    });
+
 };
